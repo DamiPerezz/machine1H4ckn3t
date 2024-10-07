174d9bc6da42bc81c20d91ce95e2629d7357f487
DamiPerezz <dpo8158@gmail.com>
2024-10-07 14:57:56 +0200

    Salida amigable del script

--- startup_script.sh
+++ startup_script.sh
@@ -2,21 +2,19 @@
 
 limpiar(){
     echo "Parando los contenedores, se paciente...";
-    sudo docker stop maquina01prueba1
-    sudo docker stop maquina01web2
+    sudo docker stop maquina01prueba1 > /dev/null 2>&1
+    sudo docker stop maquina01web2 > /dev/null 2>&1
     echo "Contenedores detenidos."
 
     echo "Eliminando contenedores"
-    sudo docker rm maquina01prueba1
-    sudo docker rm maquina01web2
-    echo "Eliminando contenedores"
-    sudo docker rm maquina01prueba1
+    sudo docker rm maquina01prueba1 
     sudo docker rm maquina01web2
 
     echo "Eliminando imagenes"
     sudo docker image rm maquina01:beta
     sudo docker image rm maquina01web:2
 
+    echo "Listo, ya puedes cerrar la terminal ;)"
    
     exit 0
 }
@@ -31,21 +29,33 @@ if [ "$(whoami)" != "root" ]; then
     exit 1
 fi
 
-echo "Creadno red virtual"
+echo "Inicando entorno, no cierres el proceso"
+echo "Creando red virtual..."
+sudo docker network create --subnet 172.18.0.0/16 DockerRed > /dev/null 2>&1
 
-sudo docker network create --subnet 172.18.0.0/16 DockerRed
+echo "Iniciando primer host " # API Backend
+sudo docker build -t maquina01:beta ./Back-end > /dev/null 2>&1
+sudo docker create --name maquina01prueba1 --net DockerRed --ip 172.18.0.5 maquina01:beta > /dev/null 2>&1
+sudo docker start maquina01prueba1 > /dev/null 2>&1
 
-echo "Iniciando primer host (Backend API)"
-sudo docker build -t maquina01:beta ./Back-end
-sudo docker create --name maquina01prueba1 --net DockerRed --ip 172.18.0.5 maquina01:beta
-sudo docker start maquina01prueba1
+echo "Iniciando segundo host  " # Servidor Web
+sudo docker build -t maquina01web:2 ./Front-end > /dev/null 2>&1
+sudo docker create --name maquina01web2 --net DockerRed --ip 172.18.0.6 maquina01web:2 > /dev/null 2>&1
+sudo docker start maquina01web2 > /dev/null 2>&1
+echo ""
+echo "Ambos contenedores se han iniciado correctamente. "
+echo "Laboratorio deplegado, para cerrarlo presiona CTRL+C y espera a que se cierre  "
+echo "-------------------------------------------------- "
+echo ""
+echo "Desplegados 2 hosts en red 172.18.0.0 "
+echo "Identificalos, atacalos y consigue 2 flags "
+echo "Buena suerte "
+echo ""
+echo "-------------------------------------------------- "
+echo ""
+echo "Machine made by dpmcyber for H4ckn3t Team :) "
+echo "Team contact: https://linktr.ee/h4ckn3t "
 
-echo "Iniciando segundo host (Servidor Web Frontend)"
-sudo docker build -t maquina01web:2 ./Front-end
-sudo docker create --name maquina01web2 --net DockerRed --ip 172.18.0.6 maquina01web:2
-sudo docker start maquina01web2
-
-echo "Ambos contenedores se han iniciado correctamente."
 
 #Para mantener el script esperando a que pulse en SIGINT
 while true; do
